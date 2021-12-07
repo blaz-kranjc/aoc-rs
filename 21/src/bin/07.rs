bd7a07dcd11e0df1b3447627b99e054e1919a20c
Blaz Kranjc <blaz@kranjc.xyz>
2021-12-07 09:52:02 +0100

    Day 7.

---
+++ 21/src/bin/07.rs
@@ -0,0 +1,30 @@
+fn sum_ints_until(n: i32) -> i32 {
+    (n * (n + 1)) / 2
+}
+
+fn main() {
+    let mut crab_positions = aoc::get_input(21, 7)
+        .trim()
+        .split(',')
+        .filter_map(|s| s.parse::<i32>().ok())
+        .collect::<Vec<_>>();
+    crab_positions.sort();
+    let linear = crab_positions
+        .iter()
+        .map(|p| crab_positions.iter().map(|x| (x - p).abs()).sum::<i32>())
+        .min()
+        .unwrap();
+    println!("Part 1: {}", linear);
+
+    let quadratic = crab_positions
+        .iter()
+        .map(|p| {
+            crab_positions
+                .iter()
+                .map(|x| sum_ints_until((x - p).abs()))
+                .sum::<i32>()
+        })
+        .min()
+        .unwrap();
+    println!("Part 2: {}", quadratic);
+}
